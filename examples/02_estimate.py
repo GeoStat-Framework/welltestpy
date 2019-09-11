38ed2b2dc9d6017224f725c8aeb9c40db48e4874
Kent Inverarity <kinverarity@hotmail.com>
2019-09-11 11:02:19 +0930

    Align example with README
    Tested on Windows, works fine, creating  graphs and outputs.

--- examples/02_estimate.py
+++ examples/02_estimate.py
@@ -2,14 +2,5 @@
 import welltestpy as wtp
 
 campaign = wtp.data.load_campaign("Cmp_UFZ-campaign.cmp")
-estimation = wtp.estimate.Theisest("Estimate_theis", campaign)
-estimation.setpumprate()
-estimation.settime()
-estimation.genrtdata()
-estimation.run(
-    dbname="database",
-    plotname1="paratrace.pdf",
-    plotname2="fit_plot.pdf",
-    plotname3="parainteract.pdf",
-    estname="estimation.txt",
-)
+estimation = wtp.estimate.Theis("Estimate_theis", campaign, generate=True)
+estimation.run()
