b952977c8100c6e491845922227bffbd5fe85045
MuellerSeb <mueller.seb@posteo.de>
2019-09-12 18:47:36 +0200

    Add triangulation example

--- examples/02_estimate.py
+++ examples/02_estimate.py
@@ -4,5 +4,4 @@ import welltestpy as wtp
 campaign = wtp.data.load_campaign("Cmp_UFZ-campaign.cmp")
 estimation = wtp.estimate.Theis("Estimate_theis", campaign, generate=True)
 estimation.run()
-# estimation.gen_setup(dummy=True)
 estimation.sensitivity()
---
+++ examples/06_triangulate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from welltestpy.tools.trilib import triangulate, sym
+from welltestpy.tools.plotter import plotres
+
+dist_mat = np.zeros((4, 4), dtype=float)
+dist_mat[0, 1] = 3  # distance between well 0 and 1
+dist_mat[0, 2] = 4  # distance between well 0 and 2
+dist_mat[1, 2] = 2  # distance between well 1 and 2
+dist_mat[0, 3] = 1  # distance between well 0 and 3
+dist_mat[1, 3] = 3  # distance between well 1 and 3
+dist_mat[2, 3] = -1  # unknown distance between well 2 and 3
+dist_mat = sym(dist_mat)  # make the distance matrix symmetric
+res = triangulate(dist_mat, prec=0.1)
+
+# plot all possible well constellations
+plotres(res)
