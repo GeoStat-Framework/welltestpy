a856331a2a90297f63c3318d62d970f125bbc5ba
MuellerSeb <mueller.seb@posteo.de>
2019-09-12 14:02:30 +0200

    update examples

--- examples/01_create.py
+++ examples/01_create.py
@@ -8,35 +8,35 @@ field = wtp.data.FieldSite(name="UFZ", coordinates=[51.353839, 12.431385])
 campaign = wtp.data.Campaign(name="UFZ-campaign", fieldsite=field)
 
 ### add 4 wells to the campaign
-campaign.add_well(name="well_0", radius=0.1, coordinates=(0., 0.))
-campaign.add_well(name="well_1", radius=0.1, coordinates=(1., -1.))
-campaign.add_well(name="well_2", radius=0.1, coordinates=(2., 2.))
-campaign.add_well(name="well_3", radius=0.1, coordinates=(-2., -1.))
+campaign.add_well(name="well_0", radius=0.1, coordinates=(0.0, 0.0))
+campaign.add_well(name="well_1", radius=0.1, coordinates=(1.0, -1.0))
+campaign.add_well(name="well_2", radius=0.1, coordinates=(2.0, 2.0))
+campaign.add_well(name="well_3", radius=0.1, coordinates=(-2.0, -1.0))
 
 ### generate artificial drawdown data with the Theis solution
-prate = -1e-4
+rate = -1e-4
 time = np.geomspace(10, 7200, 10)
 transmissivity = 1e-4
 storage = 1e-4
 rad = [
-   campaign.wells["well_0"].radius,                     # well radius of well_0
-   campaign.wells["well_0"] - campaign.wells["well_1"], # distance between 0-1
-   campaign.wells["well_0"] - campaign.wells["well_2"], # distance between 0-2
-   campaign.wells["well_0"] - campaign.wells["well_3"], # distance between 0-3
+    campaign.wells["well_0"].radius,  # well radius of well_0
+    campaign.wells["well_0"] - campaign.wells["well_1"],  # distance 0-1
+    campaign.wells["well_0"] - campaign.wells["well_2"],  # distance 0-2
+    campaign.wells["well_0"] - campaign.wells["well_3"],  # distance 0-3
 ]
 drawdown = ana.theis(
-    rad=rad,
     time=time,
-    T=transmissivity,
-    S=storage,
-    Qw=prate,
+    rad=rad,
+    storage=storage,
+    transmissivity=transmissivity,
+    rate=rate,
 )
 
 ### create a pumping test at well_0
 pumptest = wtp.data.PumpingTest(
     name="well_0",
     pumpingwell="well_0",
-    pumpingrate=prate,
+    pumpingrate=rate,
     description="Artificial pump test with Theis",
 )
 
@@ -48,6 +48,8 @@ pumptest.add_transient_obs("well_3", time, drawdown[:, 3])
 
 ### add the pumping test to the campaign
 campaign.addtests(pumptest)
+### optionally make the test steady
+# campaign.tests["well_0"].make_steady()
 
 ### plot the well constellation and a test overview
 campaign.plot_wells()
--- examples/03_estimate_hetero.py
+++ examples/03_estimate_hetero.py
@@ -2,14 +2,5 @@
 import welltestpy as wtp
 
 campaign = wtp.data.load_campaign("Cmp_UFZ-campaign.cmp")
-estimation = wtp.estimate.Stat2Dest("Estimate_theis", campaign)
-estimation.setpumprate()
-estimation.settime()
-estimation.genrtdata()
-estimation.run(
-    dbname="database_het",
-    plotname1="paratrace_het.pdf",
-    plotname2="fit_plot_het.pdf",
-    plotname3="parainteract_het.pdf",
-    estname="estimation_het.txt",
-)
+estimation = wtp.estimate.ExtTheis2D("Estimate_het2D", campaign, generate=True)
+estimation.run()
