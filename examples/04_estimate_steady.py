cd26028cdea852720f90492a23e3c2960f6c374c
MuellerSeb <mueller.seb@posteo.de>
2019-09-12 16:46:59 +0200

    Add steady estimate example [skip ci]

--- examples/04_estimate_steady.py
+++ examples/04_estimate_steady.py
@@ -2,7 +2,7 @@
 import welltestpy as wtp
 
 campaign = wtp.data.load_campaign("Cmp_UFZ-campaign.cmp")
-estimation = wtp.estimate.Theis("Estimate_theis", campaign, generate=True)
+estimation = wtp.estimate.Thiem("Estimate_thiem", campaign, generate=True)
 estimation.run()
-# estimation.gen_setup(dummy=True)
+estimation.gen_setup(dummy=True)
 estimation.sensitivity()
