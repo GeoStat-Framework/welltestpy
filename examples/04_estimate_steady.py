24d21a9d59807ba1175e5eafa6b7862cd0019010
MuellerSeb <mueller.seb@posteo.de>
2020-04-01 18:18:33 +0200

    examples: description

--- examples/04_estimate_steady.py
+++ examples/04_estimate_steady.py
@@ -4,5 +4,7 @@ import welltestpy as wtp
 campaign = wtp.data.load_campaign("Cmp_UFZ-campaign.cmp")
 estimation = wtp.estimate.Thiem("Estimate_thiem", campaign, generate=True)
 estimation.run()
+# since we only have one parameter,
+# we need a dummy parameter to estimate sensitivity
 estimation.gen_setup(dummy=True)
 estimation.sensitivity()
