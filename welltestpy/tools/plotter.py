3763ae36d7bddf2c1154ca9f20603e6b2f480533
MuellerSeb <mueller.seb@posteo.de>
2020-04-08 16:23:42 +0200

    plotter: doc update; small refactor; better ticks foll well-plots

--- welltestpy/tools/plotter.py
+++ welltestpy/tools/plotter.py
@@ -18,6 +18,7 @@ The following classes and functions are provided
    plotparatrace
    plotsensitivity
 """
+# pylint: disable=C0103
 import copy
 import warnings
 import functools as ft
@@ -37,7 +38,7 @@ def _get_fig_ax(
     sub_kwargs=None,
     **fig_kwargs
 ):  # pragma: no cover
-    # 0->None or given, 1->False, 2->True
+    # ax_case: 0->None (create one) or given, 1->False, 2->True
     ax_case = 1 + int(ax) if isinstance(ax, bool) else 0
     sub_args = (111,) if sub_args is None else sub_args
     sub_kwargs = {} if sub_kwargs is None else sub_kwargs
@@ -51,7 +52,7 @@ def _get_fig_ax(
         assert ax.get_figure() is fig
         return fig, ax
     # if ax=False we only want a figure
-    elif ax_case == 1:
+    if ax_case == 1:
         return plt.figure(**fig_kwargs) if fig is None else fig
     # if ax=True we want the current axis of the given figure
     assert fig is not None
@@ -67,7 +68,24 @@ def _sort_lgd(ax, **kwargs):
 
 
 def dashes(i=1, max_n=12, width=1):
-    """Dashes for matplotlib."""
+    """
+    Dashes for matplotlib.
+
+    Parameters
+    ----------
+    i : int, optional
+        Number of dots. The default is 1.
+    max_n : int, optional
+        Maximal Number of dots. The default is 12.
+    width : float, optional
+        Linewidth. The default is 1.
+
+    Returns
+    -------
+    list
+        dashes list for matplotlib.
+
+    """
     return i * [width, width] + [max_n * 2 * width - 2 * i * width, width]
 
 
@@ -105,21 +123,33 @@ def fadeline(ax, x, y, label=None, color=None, steps=20, **kwargs):
     kwargs["solid_capstyle"] = "butt"
 
     for i in range(steps):
-        if i == 0:
-            label0 = label
-        else:
-            label0 = None
-        ax.plot(
-            [xarr[i], xarr[i + 1]],
-            [yarr[i], yarr[i + 1]],
-            label=label0,
-            alpha=(steps - i) * (1.0 / steps) * 0.9 + 0.1,
-            **kwargs
-        )
+        kwargs["label"] = label if i == 0 else None
+        kwargs["alpha"] = (steps - i) * (1.0 / steps) * 0.9 + 0.1
+        ax.plot([xarr[i], xarr[i + 1]], [yarr[i], yarr[i + 1]], **kwargs)
 
 
 def campaign_plot(campaign, select_test=None, fig=None, style="WTP", **kwargs):
-    """Plot an overview of the tests within the campaign."""
+    """
+    Plot an overview of the tests within the campaign.
+
+    Parameters
+    ----------
+    campaign : :class:`Campaign`
+        The campaign to be plotted.
+    select_test : dict, optional
+        The selected tests to be added to the plot. The default is None.
+    fig : Figure, optional
+        Matplotlib figure to plot on. The default is None.
+    style : str, optional
+        Plot stlye. The default is "WTP".
+    **kwargs : TYPE
+        Keyword arguments forwarded to the tests plotting routines.
+
+    Returns
+    -------
+    fig : Figure
+        The created matplotlib figure.
+    """
     if select_test is None:
         tests = list(campaign.tests.keys())
     else:
@@ -151,7 +181,28 @@ def campaign_plot(campaign, select_test=None, fig=None, style="WTP", **kwargs):
 def campaign_well_plot(
     campaign, plot_tests=True, plot_well_names=True, fig=None, style="WTP"
 ):
-    """Plot of the well constellation within the campaign."""
+    """
+    Plot of the well constellation within the campaign.
+
+    Parameters
+    ----------
+    campaign : :class:`Campaign`
+        The campaign to be plotted.
+    plot_tests : bool, optional
+        DESCRIPTION. The default is True.
+    plot_well_names : TYPE, optional
+        DESCRIPTION. The default is True.
+    fig : Figure, optional
+        Matplotlib figure to plot on. The default is None.
+    style : str, optional
+        Plot stlye. The default is "WTP".
+
+    Returns
+    -------
+    ax : Axes
+        The created matplotlib axes.
+
+    """
     well_const0 = []
     names = []
 
@@ -165,6 +216,14 @@ def campaign_well_plot(
         names.append(w)
     well_const = [well_const0]
 
+    fig = plot_well_pos(
+        well_const,
+        names,
+        plot_well_names=plot_well_names,
+        fig=fig,
+        style=style,
+    )
+
     style = copy.deepcopy(plt.rcParams) if style is None else style
     keep_fs = False
     if style == "WTP":
@@ -176,13 +235,6 @@ def campaign_well_plot(
             plt.rcParams.update({"font.size": font_size})
         clrs = plt.rcParams["axes.prop_cycle"].by_key()["color"]
         clr_n = len(clrs)
-        fig = plot_well_pos(
-            well_const,
-            names,
-            campaign.name,
-            plot_well_names=plot_well_names,
-            fig=fig,
-        )
 
         fig, ax = _get_fig_ax(fig, ax=True)
 
@@ -228,8 +280,17 @@ def plot_pump_test(
     exclude: :class:`list`, optional
         List of wells that should be excluded from the plot.
         Default: ``None``
+    fig : Figure, optional
+        Matplotlib figure to plot on. The default is None.
     ax : :class:`Axes`
-        Axes where the plot should be done.
+        Matplotlib axes to plot on. The default is None.
+    style : str, optional
+        Plot stlye. The default is "WTP".
+
+    Returns
+    -------
+    ax : Axes
+        The created matplotlib axes.
 
     Notes
     -----
@@ -268,7 +329,7 @@ def plot_pump_test(
             ax1 = None
             ax2 = ax
         else:
-            return
+            raise ValueError("plot_pump_test: unknow state of pumping test.")
         for i, k in enumerate(plot_wells):
             if k != pump_test.pumpingwell:
                 dist = wells[k] - wells[pump_test.pumpingwell]
@@ -348,10 +409,37 @@ def plot_well_pos(
     title="",
     filename=None,
     plot_well_names=True,
+    ticks_set="auto",
     fig=None,
     style="WTP",
 ):
-    """Plot all well constellations and label the points with the names."""
+    """
+    Plot all well constellations and label the points with the names.
+
+    Parameters
+    ----------
+    well_const : list
+        List of well constellations.
+    names : list of str, optional
+        Names for the wells. The default is None.
+    title : str, optional
+        Plot title. The default is "".
+    filename : str, optional
+        Filename if the result should be saved. The default is None.
+    plot_well_names : bool, optional
+        Whether to plot the well-names. The default is True.
+    ticks_set : int or str, optional
+        Tick spacing in the plot. The default is "auto".
+    fig : Figure, optional
+        Matplotlib figure to plot on. The default is None.
+    style : str, optional
+        Plot stlye. The default is "WTP".
+
+    Returns
+    -------
+    fig : Figure
+        The created matplotlib figure.
+    """
     # calculate Column- and Row-count for quadratic shape of the plot
     # total number of plots
     total_n = len(well_const)
@@ -385,6 +473,19 @@ def plot_well_pos(
     space = 0.1 * max(abs(xmax - xmin), abs(ymax - ymin))
     xspace = yspace = space
 
+    if ticks_set == "auto":
+        # bit hacky auto-ticking to be more pleasant for the eyes
+        tick_list = [1, 2, 5, 10]
+        tk_space = space * 10 / 7  # assume about 7 ticks
+        scaling = np.log10(tk_space)
+        if np.log10(0.4) < scaling < 1:
+            # if space is less 10, choose nearest value in tick_list (by log)
+            ticks_set = min(tick_list, key=lambda x: abs(np.log(x / tk_space)))
+        else:
+            # k * 10 ** n as ticks (0.1, 0.2, ..., 10, 20, ..., 100, 200, ...)
+            space_pot = 10 ** int(np.floor(scaling))
+            ticks_set = space_pot * int(np.around(tk_space / space_pot))
+
     style = copy.deepcopy(plt.rcParams) if style is None else style
     keep_fs = False
     if style == "WTP":
@@ -410,15 +511,16 @@ def plot_well_pos(
                     ax.annotate(
                         "  " + name, (wells[j][0], wells[j][1]), zorder=100
                     )
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
-            ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
-            ax.set_xlabel("x distance in $[m]$")  # , fontsize=16)
-            ax.set_ylabel("y distance in $[m]$")  # , fontsize=16)
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(ticks_set))
+            ax.yaxis.set_major_locator(ticker.MultipleLocator(ticks_set))
+            ax.set_xlabel("x distance in $[m]$")
+            ax.set_ylabel("y distance in $[m]$")
             if total_n > 1:
                 ax.set_title("Result {}".format(i))
 
+        if title:
+            fig.suptitle(title)
         fig.tight_layout(rect=[0, 0, 1, 0.95])
-
         if filename is not None:
             fig.savefig(filename, format="pdf")
 
@@ -482,7 +584,7 @@ def plotfit_transient(
         xydir = np.zeros_like(time)
         test_name = list(np.unique(radnames[:, 0]))
         test_name.sort()
-        rad_unique, rad_un_idx = np.unique(rad, return_index=True)
+        __, rad_un_idx = np.unique(rad, return_index=True)
         for ri, re in enumerate(rad):
             r1 = re * r_gen
             r11 = re * r_gen1
@@ -540,7 +642,6 @@ def plotfit_transient(
             h = plot_f(**{extra["time"]: te, extra["rad"]: radarr}).reshape(-1)
             ax.plot(radarr, t11, h, color="k", alpha=0.1, linestyle="--")
 
-        # ax.view_init(elev=45, azim=155)
         ax.view_init(elev=40, azim=125)
         ax.set_xlabel(r"$r$ in $\left[\mathrm{m}\right]$")
         ax.set_ylabel(r"$t$ in $\left[\mathrm{s}\right]$")
@@ -581,7 +682,6 @@ def plotfit_steady(
 
     test_name = list(np.unique(radnames[:, 0]))
     test_name.sort()
-    rad_unique, rad_un_idx = np.unique(rad, return_index=True)
 
     style = copy.deepcopy(plt.rcParams) if style is None else style
     keep_fs = False
